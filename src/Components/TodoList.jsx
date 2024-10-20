36a0a3ffc89b795ec28e81543f40dc526a97d347
Frumosi <kkone.devops@gmail.com>
2024-10-20 12:10:54 -0400

    Updated

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -24,6 +24,13 @@ const handleAddList = (index) => {
 const handleListInputChange = (index, value) => {
     setListInputs({ ...listInputs, [index]: value });
 };
+
+
+const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
   
 
   return (
